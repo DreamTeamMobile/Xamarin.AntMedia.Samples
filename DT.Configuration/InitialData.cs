9a21df2d424ee83b4496a33a64ae2c5c9f35a0cf
Sergei Bokatuk <s.bokatuk@dreamteam-mobile.com>
2021-01-19 01:44:43 +0300

    update packages versions

--- DT.Configuration/InitialData.cs
+++ DT.Configuration/InitialData.cs
@@ -5,8 +5,8 @@ namespace DT.Configuration
     public class InitialData
     {
         public const string SERVER_ADDRESS = "antmedia.drmtm.us:5080";
-        public const string SERVER_URL = "ws://" + SERVER_ADDRESS + "/WebRTCAppEE/websocket";
-        public const string REST_URL = "http://" + SERVER_ADDRESS + "/WebRTCAppEE/rest/v2";
+        public const string SERVER_URL = "ws://" + SERVER_ADDRESS + "/LiveApp/websocket";
+        public const string REST_URL = "http://" + SERVER_ADDRESS + "/LiveApp/rest/v2";
         public const string DefaultStream = "stream1";
         public const string Token = "";
     }
